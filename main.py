305eac5f8dff15344cf5052c9671debbc05c3917
TetianaShevch <139060254+TetianaShevch@users.noreply.github.com>
2023-07-12 20:55:23 +0300

    After review commit

--- main.py
+++ main.py
@@ -14,24 +14,25 @@ import shutil
 import re
 import sort
 
-# python main.py Rubbish 
+# python main.py <Name of folder> 
 
-folder_to_scan = sys.argv[1]
-# folder_to_scan = 'Rubbish'
-          
-sort.read_folder(Path(folder_to_scan), Path(folder_to_scan)) # Обробляємо папку
+if __name__ =="__main__":
+    
+    folder_to_scan = sys.argv[1]
+              
+    sort.read_folder(Path(folder_to_scan), Path(folder_to_scan)) # Обробляємо папку
 
-sort.handle_empty_folders(Path(folder_to_scan)) # після сортування файлів у папці видаляємо пусті папки, що утворилися після упорядкування, і нормалізуємо назви файлів
+    sort.handle_empty_folders(Path(folder_to_scan)) # після сортування файлів у папці видаляємо пусті папки, що утворилися після упорядкування, і нормалізуємо назви файлів
 
-# Друкуємо список файлів у категорії (музика, відео, фото та ін.)
-print(f'Images: {sort.images}') 
-print(f'Video: {sort.video}')
-print(f'Documents: {sort.documents}')
-print(f'Audio: {sort.audio}')
-print(f'Archives: {sort.archives}')
-print(f'Other files: {sort.my_others}')
-# Друкуєхмо перелік всіх відомих скриптів розширень, які зустрічаються в цільовій папці.
-print(f'Types of files in folder: {sort.EXTENSION}')
-# Друкуємо перелік усіх розширень, які скрипту невідомі.
-print(f'Files of Unknown types: {sort.unknown}')
+    # Друкуємо список файлів у категорії (музика, відео, фото та ін.)
+    print(f'Images: {sort.images}') 
+    print(f'Video: {sort.video}')
+    print(f'Documents: {sort.documents}')
+    print(f'Audio: {sort.audio}')
+    print(f'Archives: {sort.archives}')
+    print(f'Other files: {sort.my_others}')
+    # Друкуєхмо перелік всіх відомих скриптів розширень, які зустрічаються в цільовій папці.
+    print(f'Types of files in folder: {sort.EXTENSION}')
+    # Друкуємо перелік усіх розширень, які скрипту невідомі.
+    print(f'Files of Unknown types: {sort.unknown}')
 
